20c8fdf198ddfde67e27a9f440e1d9564364b25a
Martin Powers <martinpwrs@gmail.com>
2019-04-13 15:39:01 -0600

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+eval (python3 -m virtualfish auto_activation)
+set -g theme_nerd_fonts yes
+
+alias gb="git branch"
+alias gd="git diff"
+alias gl="git log"
+alias gs="git status"
+alias gco="git checkout"
+alias gc="git commit"
+
+alias cdd="cd ~/Downloads/"
+alias lr="ls -lR"
